20f00b9fc8abf008975796f7376cd2f04616a899
Jose Claudio Tomas Junior <jclaudiotomas@gmail.com>
2021-05-14 17:28:32 -0300

    criando função de busca de ips

--- app/app.go
+++ app/app.go
@@ -1,6 +1,10 @@
 package app
 
 import (
+	"fmt"
+	"log"
+	"net"
+
 	"github.com/urfave/cli"
 )
 
@@ -10,6 +14,32 @@ func Gerar() *cli.App {
 	app.Name = "Aplicação de linha de comando"
 	app.Usage = "Busca de Ips e Nome de servidor na interner"
 
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Busca Ips de endereções na Internet",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "gobyexample.com",
+				},
+			},
+			Action: buscarIps,
+		},
+	}
 	return app
 
+}
+
+func buscarIps(c *cli.Context) {
+	host := c.String("host")
+
+	ips, erro := net.LookupIP(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
